8e5f634bf60e1c99e2301c3254deac4a062f38bf
Jeremy Salwen <jeremybubs@gmail.com>
2010-12-21 23:46:13 -0500

    Worked on debian structure

--- debian/aubio-lv2.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the aubio-lv2 package
-#
-0 4	* * *	root	[ -x /usr/bin/aubio-lv2_maintenance ] && /usr/bin/aubio-lv2_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(aubio-lv2):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="aubio-lv2" command="/usr/bin/aubio-lv2"
